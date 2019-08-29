7a3b53649dfd8d7423695055fcd4185cb8dd8dee
Deepti Chitte <deeptichitte@gmail.com>
2019-08-29 14:46:13 -0400

    UI for time-is-money app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { HomeComponent } from './home/home.component';
+import {SignInComponent} from './sign-in/sign-in.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  '', pathMatch:  'full', redirectTo:  'home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'contact-create', component: ContactCreateComponent},
+  {path: 'contact-list', component: ContactListComponent},
+  {path: 'sign-in', component: SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactCreateComponent,
+    ContactListComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
